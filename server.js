ebfb74dc0fa5ec47f9018aac0137c06869b545e4
Shreeviknesh <luckyboy1998@gmail.com>
2018-07-29 11:54:46 +0530

    Basic structure laid down

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const kural = express();
+
+//Connecting to MongoDB
+const { mongoURI } = require('./config/keys');
+mongoose.connect(mongoURI, { useNewUrlParser: true })
+    .then( () => console.log("MongoDB connected"))
+    .catch( err => console.log(err));
+
+//Defining the Routes
+const loginRoute = require('./routes/login');
+kural.use('/login', loginRoute);
+
+const registerRoute = require('./routes/register');
+kural.use('/register', registerRoute);
+
+//Listen to port
+const port = process.env.PORT || 5500;
+kural.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
