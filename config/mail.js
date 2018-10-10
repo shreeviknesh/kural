bad3c2ce961a95532317bbe363b825c0feb923cf
Shreeviknesh <luckyboy1998@gmail.com>
2018-10-10 19:57:26 +0530

    Minor Changes

--- config/mail.js
+++ config/mail.js
@@ -1,7 +1,9 @@
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 var nodemailer = require('nodemailer');
 var temporaryPassword = require('uuid/v4')();
 
-module.exports = sendMail = email => {
+module.exports = sendMail = user => {
   var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -10,9 +12,21 @@ module.exports = sendMail = email => {
     }
   });
 
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(temporaryPassword, salt, function(err, hash) {
+        if(err)
+          throw err;
+        User.update({"userID": user.userID}, {$set: {"password": hash}}, (err, res) => {
+        if(err) throw err;
+        else
+          console.log("success: "+res);
+      });
+    });
+  });
+
   var options = {
     from: 'Kural',
-    to: email,
+    to: user.email,
     subject: 'Password reset request for Kural account',
     text: `Please use the following Password to login: ${temporaryPassword}`
   };
